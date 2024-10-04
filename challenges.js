424bfce824b471b82d172ee3a552b94a8a716197
Thunder <Thunderbot1994@gmail.com>
2024-10-04 19:32:22 +0800

    v0.0.5
    Added Refresh Button
    Changed Table To Be More Reliable
    Added Unique Challenges

---
+++ challenges.js
@@ -0,0 +1,41 @@
+data = {
+    Easy:[
+        "Brush",
+        "Cactus",
+        "Enter The Nether",
+        ""
+    ],
+    Normal:[
+        "Bucket of Pufferfish",
+        "Heart of the Sea",
+        "Recovery Compass",
+        "Honey Block",
+        "Diamond",
+        "Break 10 Obsidian",
+        "Amethyst Shard",
+        ""
+    ],
+    Hard:[
+        "4 Diamond Blocks",
+        "Netherite Block",
+        "Enchanted Golden Apple",
+        "Reach Level 30",
+        "Mace",
+        "Trident",
+        "Conduit",
+        "Dragon Egg",
+        "Elytra",
+        "Dragon Head",
+        "Beacon",
+        "Shulker Box",
+        "Break a Diamond Hoe",
+        "Respawn the Enderdragon",
+        "Turtle Helmet"
+    ],
+    Insane:[
+        "Fully Power a Beacon",
+        "Obtain All Mob Heads",
+        "Achieve How Did We Get Here Advancement",
+        ""
+    ]
+}
--- code.js
+++ code.js
@@ -6,41 +6,79 @@ function generate(input) {
     }
 }
 
-function buttons() {
+function buttons_init() {
     for (let i = 0; i < 5; i++) {
         for (let j = 0; j < 5; j++) {
-            document.getElementById("table").children[0].children[0].children[i].children[j].children[0].onclick = function() {button(document.getElementById("table").children[0].children[0].children[i].children[j].children[0])};
+            var dabutton = document.getElementById("table").children[0].children[0].children[i].children[j].children[0];
+            dabutton.onclick = function() {button(document.getElementById("table").children[0].children[0].children[i].children[j].children[0])};
+            document.getElementById("table").children[0].children[0].children[i].children[j].children[0].addEventListener("mouseover", () => {
+                document.getElementById("table").children[0].children[0].children[i].children[j].children[0].style.backgroundColor = "#9E9E9E";
+                document.getElementById("table").children[0].children[0].children[i].children[j].style.backgroundColor = "#9E9E9E";
+            });
+            document.getElementById("table").children[0].children[0].children[i].children[j].children[0].addEventListener("mouseout", () => {
+                document.getElementById("table").children[0].children[0].children[i].children[j].children[0].style.backgroundColor = "#8B8B8B";
+                document.getElementById("table").children[0].children[0].children[i].children[j].style.backgroundColor = "#8B8B8B";
+            });
         }
     }
+    document.getElementById("Refresh").onclick = Refresh;
+    document.getElementById("Refresh").addEventListener("mouseover", () => {
+        document.getElementById("Refresh.text").style.color = "#FFFF55";
+        document.getElementById("Refresh.text").style.textShadow = "2px 2px #3F3F15";
+    });
+    document.getElementById("Refresh").addEventListener("mouseout", () => {
+        document.getElementById("Refresh.text").style.color = "#FFFFFF";
+        document.getElementById("Refresh.text").style.textShadow = "2px 2px #3F3F3F";
+    });
 }
 
 function button(dabutton) {
     if (dabutton.style.backgroundColor == "rgb(85, 255, 85)" || dabutton.style.backgroundColor == "rgb(160, 255, 160)") {
-        dabutton.style.backgroundColor = "#9E9E9E";  
+        dabutton.style.backgroundColor = "#9E9E9E";
+        dabutton.parentElement.style.backgroundColor = "#9E9E9E";
         dabutton.addEventListener("mouseover", () => {
-            dabutton.style.backgroundColor = "#9E9E9E"
-        })      
+            dabutton.style.backgroundColor = "#9E9E9E";
+            dabutton.parentElement.style.backgroundColor = "#9E9E9E";
+        });   
         dabutton.addEventListener("mouseout", () => {
-            dabutton.style.backgroundColor = "#8B8B8B"
-        })      
+            dabutton.style.backgroundColor = "#8B8B8B";
+            dabutton.parentElement.style.backgroundColor = "#8B8B8B";
+        }); 
     }
     else {
         dabutton.style.backgroundColor = "#A0FFA0";
+        dabutton.parentElement.style.backgroundColor = "#A0FFA0";
         dabutton.addEventListener("mouseover", () => {
-            dabutton.style.backgroundColor = "#A0FFA0"
-        })      
+            dabutton.style.backgroundColor = "#A0FFA0";
+            dabutton.parentElement.style.backgroundColor = "#A0FFA0";
+        });
         dabutton.addEventListener("mouseout", () => {
-            dabutton.style.backgroundColor = "#55FF55"
-        })   
+            dabutton.style.backgroundColor = "#55FF55";
+            dabutton.parentElement.style.backgroundColor = "#55FF55";
+        });
     }
 }
 
-var text = [
-    ["I love you!","","","",""],
-    ["","","","",""],
-    ["","","","",""],
-    ["","","","",""],
-    ["","","","",""]];
+function Refresh() {    
+    var text = [
+        ["I love","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""]];
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 5; j++) {            
+            switch (2) {
+                case 0: text[i][j] = data.Easy[(Math.floor(Math.random() * 100)) % 3]; break;
+                case 1: text[i][j] = data.Normal[(Math.floor(Math.random() * 100)) % 7]; break;
+                case 2: text[i][j] = data.Hard[(Math.floor(Math.random() * 100)) % 15]; break;
+                case 3: text[i][j] = data.Insane[(Math.floor(Math.random() * 100)) % 3]; break;
 
-generate(text);
-buttons();
+            }
+        }
+    }
+    generate(text);
+}
+
+Refresh();
+buttons_init();
